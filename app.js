1ae8b754ee445166bebd12e22b89e3ec4af0d012
cat-macedo <catarina.scabelli@gmail.com>
2025-01-29 14:21:53 -0300

    cria lista de amigos

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-let listaAmigos = [];
+let arrayAmigos = [];
 let campo = document.getElementById('amigo');
+let listaAmigos = document.getElementById('listaAmigos');
 
 function adicionarAmigo() {
     if(campo.value == '') {
         alert('Por favor, insira um nome.');
     } else {
         let nome = campo.value;
-        listaAmigos.push(nome); 
+        arrayAmigos.push(nome); 
         limparCampo();
-        console.log(listaAmigos);
+        addLista();
+    }
+}
+
+function addLista() {
+    listaAmigos.innerHTML = '';
+    let tamLista = arrayAmigos.length;
+
+    for(let i=0; i<tamLista; i++){
+        const li = document.createElement('li');
+        li.textContent = arrayAmigos[i];
+        listaAmigos.appendChild(li);
     }
 }
 
